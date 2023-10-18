38a445768764defebc6850befa05ec1e7f1cadf4
nebnostaw <benjamin.watson@flocksafety.com>
2023-10-18 10:23:33 -0400

    Minor updates

--- core/analysis/rules/vuln/serializable_rule.py
+++ core/analysis/rules/vuln/serializable_rule.py
@@ -6,8 +6,12 @@ from core.analysis import Analyzed
 from core.analysis.rules.base import DetectionRule
 from core.logging import info, detect
 
+SERIALIZABLE = "Serializable"
+ABSTRACT = "abstract"
+
 
 class SerializableRule(DetectionRule):
+
     def detect(self, obj: Analyzed) -> Any:
         info(f"{self.__class__.__name__} running detection")
         dalvik = obj.get_dalvik()
@@ -16,9 +20,9 @@ class SerializableRule(DetectionRule):
                 c: ClassDefItem
                 for c in item.get_classes():
                     for i in c.get_interfaces():
-                        if "Serializable" in i:
+                        if SERIALIZABLE in i:
                             m: EncodedMethod
                             for m in c.get_methods():
-                                if m.get_code_off() == 0 and "abstract" not in c.get_access_flags_string():
+                                if m.get_code_off() == 0 and ABSTRACT not in c.get_access_flags_string():
                                     detect(f"Found native method => "
                                            f"{m.get_name()}() in Serializable class {c.get_name()}")
